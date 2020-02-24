053bbcdfb7cc2c2143301c87518334240d7a35bd
CloudsColors <andreas.mansson@live.com>
2020-02-24 16:34:10 +0100

    Hello world example

---
+++ dashboard.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+
+#import os
+
+app = Flask(__name__, template_folder="./view/")
+
+@app.route('/')
+def hello_world():
+    return render_template("index.html", name="Kamil")
