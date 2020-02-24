596f2b0b08fbbc5064aeaff366bd57d45d4195f3
CloudsColors <andreas.mansson@live.com>
2020-02-24 16:39:02 +0100

    Hello world example fixed

--- dashboard.py
+++ dashboard.py
@@ -7,3 +7,6 @@ app = Flask(__name__, template_folder="./view/")
 @app.route('/')
 def hello_world():
     return render_template("index.html", name="Kamil")
+
+if __name__ == "__main__":
+    app.run( debug=True)
