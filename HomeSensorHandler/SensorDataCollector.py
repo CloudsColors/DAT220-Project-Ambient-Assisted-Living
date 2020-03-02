5c19882434815cd4834f38ca25764696bed296a6
CloudsColors <andreas.mansson@live.com>
2020-03-02 09:46:00 +0100

    Kamil added SensorDataCollector and HomeAccessFolder with contents, doorwindow csv and updated security and dashboard

--- HomeSensorHandler/SensorDataCollector.py
+++ HomeSensorHandler/SensorDataCollector.py
@@ -1,41 +1,40 @@
 import paho.mqtt.client as mqttclient
 
-import os
-import csv
+import os, csv
 
 '''
-Module for mimicing how the "database" stores the sensor information that is sent over MQTT.
+Module for collecting sensor data sent over MQTT
 '''
 def on_connect(client, userdata, flags, rc):
-    client.subscribe([("home/door/maindoor", 0),
-                      ("home/door/backdoor", 1),
-                      ("home/window/bedroom", 2),
-                      ("home/window/livingroom", 3),
-                      ("home/window/kitchen", 4)
+    client.subscribe([("home/door/maindoor"),
+                      ("home/door/backdoor"),
+                      ("home/window/bedroom"),
+                      ("home/window/livingroom"),
+                      ("home/window/kitchen")
                       ])
 
 def on_message(client, userdata, msg):
     if(msg.topic == "home/door/maindoor"):
-        writeToCsv(0,0, msg.payload.decode())
+        writeToCsv(1,0, msg.payload.decode())
 
     if(msg.topic == "home/door/backdoor"):
         writeToCsv(1,1, msg.payload.decode())
 
     if(msg.topic == "home/window/bedroom"):
-        writeToCsv(2,2, msg.payload.decode())
+        writeToCsv(1,2, msg.payload.decode())
 
     if(msg.topic == "home/window/livingroom"):
-        writeToCsv(3,3, msg.payload.decode())
+        writeToCsv(1,3, msg.payload.decode())
 
     if(msg.topic == "home/window/kitchen"):
-        writeToCsv(4,4, msg.payload.decode())
+        writeToCsv(1,4, msg.payload.decode())
 
 
 def writeToCsv(x,y, payload):
-    r = csv.reader(open(os.path.dirname(__file__)+"/data/doorwindowdata.csv"))
+    r = csv.reader(open(os.path.dirname(__file__)+"/../DataStorage/data/doorwindowdata.csv"))
     lines = list(r)
     lines[x][y] = payload
-    writer = csv.writer(open(os.path.dirname(__file__)+"/data/doorwindowdata.csv", 'w'))
+    writer = csv.writer(open(os.path.dirname(__file__)+"/../DataStorage/data/doorwindowdata.csv", 'w'))
     writer.writerows(lines)
 
 def main():
@@ -45,5 +44,6 @@ def main():
     client.connect("localhost")
     client.loop_forever()
 
+# This below for debugging
 if __name__ == "__main__":
     main()
