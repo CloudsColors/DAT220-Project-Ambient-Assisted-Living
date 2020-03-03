ff58121f7646c28542b003261398bfc7dc93f587
CloudsColors <andreas.mansson@live.com>
2020-03-03 11:39:33 +0100

    added window and door manager as well as homestatus with the logic

--- HomeAccess/HomeStatus.py
+++ HomeAccess/HomeStatus.py
@@ -1,29 +1,14 @@
-import paho.mqtt.client as mqttclient
+import csv, os
 
-import random, time
+def get_doors():
+    f = csv.reader(open(os.path.dirname(__file__)+"/../DataStorage/data/doorwindowdata.csv", "r"))
+    doorData = list(f)
+    return [[doorData[0][0], doorData[1][0]], [doorData[0][1],doorData[1][1]]]
 
-'''
-Mimics all the sensors in the house.
-'''
-def createAndRunPublishers():
-    while(True):
-        client = mqttclient.Client("HomeSensors")
-        client.connect("localhost")
-        smokeAlarm = 0
-        #Doors
-        client.publish("home/door/maindoor", f"{random.randint(0,1)}")
-        client.publish("home/door/backdoor", f"{random.randint(0,1)}")
-        #Windows
-        client.publish("home/window/bedroom", f"{random.randint(0,1)}")
-        client.publish("home/window/livingroom", f"{random.randint(0,1)}")
-        client.publish("home/window/kitchen", f"{random.randint(0,1)}")
-        #Sensors (smoke)
-        if(random.randint(1,100) > 80):
-            smokeAlaram = 1
-        else:
-            smokeAlarm = 0
-        client.publish("home/sensors/smoke", f"{smokeAlarm}")
-        time.sleep(1)
+def get_windows():
+    f = csv.reader(open(os.path.dirname(__file__)+"/../DataStorage/data/doorwindowdata.csv", "r"))
+    windowData = list(f)
+    return [[windowData[0][2], windowData[1][2]], [windowData[0][3], windowData[1][3]], [windowData[0][4], windowData[1][4]]]
 
 if __name__ == "__main__":
-    createAndRunPublishers()
+    print(get_windows())
